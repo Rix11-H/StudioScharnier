816e485bcaac10b47583933814f0cbf99d04ecd2
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-04-20 15:56:01 +0200

    feat: update navigation

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once("bootstrap.php");
 
 
 ?><!DOCTYPE html>
@@ -12,10 +13,14 @@
     <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
+
     <?php include_once("nav.inc.php"); ?>
 
-    <main>
+    <main class="landing">
+        <img src="./assets/imgs/istockphoto-659020452-170667a.png" alt="paal">
     </main>
+
     <?php include_once("footer.inc.php"); ?>
+
 </body>
 </html>
--- nav.inc.php
+++ nav.inc.php
@@ -1,3 +1,13 @@
 <nav>
-    <h1 style="font-family: Bauhaus;" >Studio Scharnier</h1>
+    <div class="nav__front">
+        <a href="#"><h1 style="font-family: Bauhaus;" >Studio Scharnier</h1></a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+    <div class="nav__back">
+        <a href="#">login</a>
+        <a href="#">signup</a>
+    </div>
 </nav>
