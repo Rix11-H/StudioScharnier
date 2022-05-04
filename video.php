a6d8c14ae0f4e5b8e6cf2649618d13359822fef9
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-05-04 14:12:25 +0200

    add: migrate css to scss files

--- video.php
+++ video.php
@@ -76,7 +76,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Studio Scharnier</title>
-    <link rel="stylesheet" href="./css/styles.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
