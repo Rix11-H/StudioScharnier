14ea42229328214c2049e6a16701538af76db244
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-05-04 16:03:23 +0200

    feat: add button styles

--- Includes/nav.inc.php
+++ Includes/nav.inc.php
@@ -1,13 +1,39 @@
 <nav>
-    <div class="nav__front">
-        <a href="index.php"><h1 class="nav__logo">Studio Scharnier</h1></a>
-        <a href="radio.php">Radio</a>
-        <a href="video.php">Video</a>
-        <a href="beeld.php">Beeld</a>
-        <a href="texts.php">Teksten</a>
+    <div class="hideDesktop">
+        <div class="nav__front">
+            <a href="index.php" class="nav__logo">
+                <h1>Studio Scharnier</h1>
+            </a>
+            <div class="dropdown">
+                <button class="button button--ghost dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a href="radio.php" class="nav__link">Radio</a>
+            <a href="video.php" class="nav__link dropdown-item">Video</a>
+            <a href="beeld.php" class="nav__link dropdown-item">Beeld</a>
+            <a href="texts.php" class="nav__link dropdown-item">Teksten</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="nav__back">
+            <a href="#" class="button button--primary"><span class="button__text">Login</span></a>
+        </div>
     </div>
-    <div class="nav__back">
-        <a href="#">login</a>
-        <a href="#">signup</a>
+
+    <div class="hideMobile">
+        <div class="nav__front">
+            <a href="index.php" class="nav__logo">
+                <h1>Studio Scharnier</h1>
+            </a>
+            <a href="radio.php" class="nav__link">Radio</a>
+            <a href="video.php" class="nav__link">Video</a>
+            <a href="beeld.php" class="nav__link">Beeld</a>
+            <a href="texts.php" class="nav__link">Teksten</a>
+        </div>
+        <div class="nav__back">
+            <a href="#" class="button button--primary"><span class="button__text">Login</span></a>
+        </div>
     </div>
 </nav>
